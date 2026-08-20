5229e8ebe4108210621b1ffc92f541c613be4ebf
danial <mdanialfarokhi@gmail.com>
2026-08-20 11:02:36 +0330

    import libraries

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, SimpleRNN
+import matplotlib.pyplot as plt
+import os
